c6f50f60b32154f7c02a5ef4a8820d424b125d2a
Jérôme <buteljerome@yahoo.fr>
2018-10-19 11:29:49 +0200

    Refactoring run-length-encoding code to respect DRY principle

--- php/run-length-encoding/run-length-encoding.php
+++ php/run-length-encoding/run-length-encoding.php
@@ -8,21 +8,14 @@
 function encode(string $input): string
 {
     $result = '';
-    if (empty($input)) return $result;
-
-    $input = str_split($input);
     $repetitions = 0;
 
-    for ($i = 0; $i < count($input); $i++) {
+    for ($i = 0; $i < strlen($input); $i++) {
         $repetitions++;
 
-        if (($i+1) < count($input)) {
-            if ($input[$i+1] !== $input[$i]) {
-                $result .= ($repetitions > 1) ? $repetitions.$input[$i] : $input[$i];
-                $repetitions = 0;
-            }
-        } else {
-             $result .= ($repetitions > 1) ? $repetitions.$input[$i] : $input[$i];
+        if ($i === strlen($input) - 1 || $input[$i+1] !== $input[$i]) {
+            $result .= ($repetitions > 1) ? $repetitions.$input[$i] : $input[$i];
+            $repetitions = 0;
         }
     }
 
@@ -37,9 +30,8 @@ function encode(string $input): string
 function decode(string $input): string
 {
     $number = $result = '';
-    $input = str_split($input);
 
-    for ($i = 0; $i < sizeof($input); $i++) {
+    for ($i = 0; $i < strlen($input); $i++) {
         if (is_numeric($input[$i])) {
             $number .= $input[$i];
         } else if (!is_numeric($input[$i])) {
