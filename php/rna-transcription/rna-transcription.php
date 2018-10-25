8bcdc01170081d3023db6a1b1cc198fa48a3fadb
Jérôme <buteljerome@yahoo.fr>
2018-10-25 09:45:41 +0200

    Updated PHP rna-transcription exercise - Using strtr() function

--- php/rna-transcription/rna-transcription.php
+++ php/rna-transcription/rna-transcription.php
@@ -7,24 +7,6 @@
  */
 function toRna(string $strand): string
 {
-    $strand = str_split($strand);
-
-    for ($i = 0; $i < count($strand); $i++) {
-        switch($strand[$i]) {
-            case 'G':
-                $strand[$i] = 'C';
-                break;
-            case 'C':
-                $strand[$i] = 'G';
-                break;
-            case 'T':
-                $strand[$i] = 'A';
-                break;
-            case 'A':
-                $strand[$i] = 'U';
-                break;
-        }
-    }
-
-    return implode('', $strand);
+    $trans = ["G" => "C", "C" => "G", "T" => "A", "A" => "U"];
+    return strtr($strand, $trans);
 }
