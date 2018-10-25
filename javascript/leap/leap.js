7cdfdbfce0510dee0caf404dc9b33dd7d93c5ccc
Jérôme <buteljerome@yahoo.fr>
2018-10-25 10:37:38 +0200

    Updated JS leap exercise - Using De Morgan's law - Avoiding useless ternary operator

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,1 +1,1 @@
-export const isLeap = (year) => (year % 4 === 0) ? (!(year % 100 === 0 && year % 400 !== 0)) : false;
+export const isLeap = year => year % 4 === 0 && year % 100 !== 0 || year % 400 === 0;
