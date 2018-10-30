a82bfb039227c9e63aa9060bbce087bb7f9d2e5b
Jérôme <buteljerome@yahoo.fr>
2018-10-30 23:27:01 +0100

    Updated PHP raindrops exercise - Used associative array and foreach loop for better maintainability

--- php/raindrops/raindrops.php
+++ php/raindrops/raindrops.php
@@ -8,10 +8,13 @@
 function raindrops(int $int): string
 {
     $output = '';
+    $sounds = [3 => 'Pling', 5 => 'Plang', 7 => 'Plong'];
 
-    if ($int % 3 === 0) $output .= 'Pling';
-    if ($int % 5 === 0) $output .= 'Plang';
-    if ($int % 7 === 0) $output .= 'Plong';
+    foreach ($sounds as $nb => $sound) {
+        if ($int % $nb === 0) {
+            $output .= $sound;
+        }
+    }
 
-    return $output === '' ? strval($int) : $output;
+    return $output === '' ? (int) $int : $output;
 }
